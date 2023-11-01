18f738480ac92f332056d1088178fcf277ecff72
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-10-31 22:49:33 -0400

    Adicionar arquivos de projeto.

---
+++ csharp-exercise-list/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_list
+{
+    internal class Employee
+    {
+        public int Id { get; private set; }
+        private string name;
+        private double salary;
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            this.salary += salary * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"name:{this.name}  salary:{this.salary}  id:{this.Id}";
+        }
+    }
+}
---
+++ csharp-exercise-list/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using csharp_exercise_list;
+using System.Collections.Generic;
+
+namespace csharpExerciseList {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de Funcionário");
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> employeeList = new List<Employee>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite o ID do Funcionário");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o nome do Funcionário");
+                string name = Console.ReadLine();
+                Console.WriteLine("Digite o salario do Funcionário");
+                double salary = double.Parse(Console.ReadLine());
+                Employee employee = new Employee(id, name, salary);
+                employeeList.Add(employee);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("digite o id do funcionario para aumento");
+            int idEmployee = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a porcetagem para o aumento");
+            double increase = double.Parse(Console.ReadLine());
+            foreach (var item in employeeList)
+            {
+                if (item.Id == idEmployee)
+                {
+                    item.increaseSalary(increase);
+                }
+            }
+
+            foreach (var item in employeeList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
